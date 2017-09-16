f9daa099f439bb687481f542eae0836f4114f38a
DOAN\huydoan <doannh@ntex.vn>
2017-09-16 14:23:20 +0700

    prototype parttern

--- AllThing/src/main/java/annotationInterface/TestClass.java
+++ AllThing/src/main/java/annotationInterface/TestClass.java
@@ -14,3 +14,4 @@ public class TestClass {
         return this;
     }
 }
+
---
+++ AllThing/src/main/java/prototype/VerhicalFactory.java
@@ -0,0 +1,18 @@
+package prototype;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class VerhicalFactory {
+    private static final Map<String, IVerhical> prototypes = new HashMap<>();
+
+    static {
+        prototypes.put("martin", new Bicycle("Martin"));
+        prototypes.put("dream", new Motobike("dream"));
+        prototypes.put("Audi", new Car("audi"));
+    }
+    //
+    public IVerhical getPrototypes(String type){
+        return prototypes.get(type).createClone();
+    }
+}
