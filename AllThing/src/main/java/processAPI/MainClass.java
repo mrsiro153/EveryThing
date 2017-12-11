43ca2aba10feda49730d6ff7e3183967a1a9c490
DESKTOP-USU3RAD\DoanNH <doannh@ntex.vn>
2017-12-11 17:35:37 +0700

    process API

--- AllThing/src/main/java/processAPI/MainClass.java
+++ AllThing/src/main/java/processAPI/MainClass.java
@@ -8,21 +8,57 @@ import java.util.concurrent.Future;
 public class MainClass {
 
     public static void main(String[] args) throws Exception{
+        MainClass m = new MainClass();
+        m.currentPercentRam();
+    }
+    //
+    public void processDemo() throws Exception{
         Process proc = Runtime.getRuntime().exec(new String[] {
                 "/bin/sh",
                 "-c",
                 "echo $PPID"
         });
+        String pid = printToConsole(proc);
+        System.out.println("Your pid is " + pid);
 
-        if (proc.waitFor() == 0) {
-            InputStream in = proc.getInputStream();
-            int available = in.available();
-            byte[] outputBytes = new byte[available];
-
-            in.read(outputBytes);
-            String pid = new String(outputBytes);
-
-            System.out.println("Your pid is " + pid);
-        }
     }
+    //
+    public void currentPercentOfCPU()throws Exception{
+        Process proc = Runtime.getRuntime().exec(new String[] {
+                "/bin/sh",
+                "-c",
+                "grep 'cpu ' /proc/stat | awk '{usage=($2+$4)*100/($2+$4+$5)} END {print usage \"%\"}'"
+                //"echo $PPID"
+        });
+        String s = printToConsole(proc);
+        System.out.println("Your %CPU is: "+s);
+    }
+    public String printToConsole(Process proc){
+        try {
+            if (proc.waitFor() == 0) {
+                InputStream in = proc.getInputStream();
+                int available = in.available();
+                byte[] outputBytes = new byte[available];
+
+                in.read(outputBytes);
+                String pid = new String(outputBytes);
+
+                return pid;
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return "";
+    }
+    //
+    public void currentPercentRam()throws Exception{
+        Process proc = Runtime.getRuntime().exec(new String[] {
+                "/bin/sh",
+                "-c",
+                "free -m | grep Mem | awk '{usage=($7*100/$2)} END {print usage \"%\"}'"
+        });
+        String s = printToConsole(proc);
+        System.out.println("Your %FreeRam is: "+s);
+    }
+
 }
--- AllThing/src/main/java/vertxHealth/MainClass.java
+++ AllThing/src/main/java/vertxHealth/MainClass.java
@@ -9,25 +9,42 @@ import io.vertx.ext.healthchecks.HealthChecks;
 import io.vertx.ext.healthchecks.Status;
 import io.vertx.ext.web.Router;
 
+import java.io.InputStream;
+
 public class MainClass {
     //
+    static Vertx vertx;
+
     public static void main(String[] args) {
-        Vertx vertx = Vertx.vertx();
+        MainClass m = new MainClass();
+        m.mainFunction();
+    }
+
+    public void processAPI() {
+        Runtime r = Runtime.getRuntime();
+        System.out.println("total memory: " + r.totalMemory());
+        System.out.println("free memory: " + r.freeMemory());
+        r.gc();
+        System.out.println("::Memory status::");
+        System.out.println("Total memory: " + r.totalMemory());
+        System.out.println("Free memory: " + r.freeMemory());
+        System.out.println("----------------------------------");
+    }
+
+    public void mainFunction() {
+        vertx = Vertx.vertx();
         HealthCheckHandler healthCheckHandler = HealthCheckHandler.create(vertx);
         healthCheckHandler.register("add", future -> {
             //future.complete(Status.KO(new JsonObject().put("memory","2mb")));
-            future.complete();
+            processAPI();
+            future.fail("donot know");
         });
-        HealthCheckHandler healthCheckHandler1 = HealthCheckHandler.createWithHealthChecks(HealthChecks.create(vertx));
-        healthCheckHandler1.register("add", future -> {
-            future.complete();
-        });
+        //
         //
         HttpServer server = vertx.createHttpServer(new HttpServerOptions().setHost("localhost").setPort(8080));
         Router router = Router.router(vertx);
         server.requestHandler(router::accept);
         router.get("/test").handler(healthCheckHandler);
-        router.get("/abcd").handler(healthCheckHandler1);
         server.listen(event -> {
             if (event.succeeded()) {
                 System.out.println("OK");
@@ -35,5 +52,39 @@ public class MainClass {
                 System.out.println("failed");
             }
         });
+    }
+
+    public void currentPercentOfCPU() throws Exception {
+        String[] command =  new String[](
+                "/bin/sh",
+                "-c",
+                "grep 'cpu ' /proc/stat | awk '{usage=($2+$4)*100/($2+$4+$5)} END {print usage \"%\"}'"
+                //"echo $PPID"
+        );
+    }
+
+    public void currentPercentRam() {
+        Process proc = Runtime.getRuntime().exec(new String[]{
+                "/bin/sh",
+                "-c",
+                "free -m | grep Mem | awk '{usage=($7*100/$2)} END {print usage \"%\"}'"
+        });
+        String s = printToConsole(proc);
+        System.out.println("Your %FreeRam is: " + s);
+    }
+
+    public String runCMDCommand(String[] commands) throws Exception {
+        Process proc = Runtime.getRuntime().exec(commands);
+
+        if (proc.waitFor() == 0) {
+            InputStream in = proc.getInputStream();
+            int available = in.available();
+            byte[] outputBytes = new byte[available];
+            in.read(outputBytes);
+            String result = new String(outputBytes);
+            System.out.println("Result is: " + result);
+            return result;
+        }
+        return "";
     }
 }
