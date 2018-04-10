62b9393c6a0995141b554f3cf095f55142940d5c
Unknown <13110034@student.hcmute.edu.vn>
2018-04-10 22:38:26 +0700

    Change gradle build

---
+++ AllThing/src/main/java/timmerDemo/MainClass.java
@@ -0,0 +1,17 @@
+package timmerDemo;
+
+import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class MainClass {
+    public static void main(String[] args) {
+        Timer t = new Timer();
+        t.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                System.out.println("Each 2mnt, I will print: "+new Date().toString());
+            }
+        }, 1000, 120000);
+    }
+}
